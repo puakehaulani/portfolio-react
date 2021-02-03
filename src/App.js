7cb9307111633d678ad6bf9168c1dbd48641de90
puakehaulani <puakehaulani@gmail.com>
2021-02-02 16:08:56 -0800

    added components to App for rendering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Container from "./components/Container";
+import Navtabs from "./components/Navtabs";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Resume from "./pages/Resume";
+import Footer from "./components/Footer";
 
 
 function App() {
   return (
-    <>
-
-    </>
+    <Router>
+      <>
+        <Container>
+          <Navtabs />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/projects" component={Projects} />
+          <Route path="/resume" component={Resume} />
+          <Footer />
+        </Container>
+      </>
+    </Router>
   );
 }
 
