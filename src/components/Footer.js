d3a49d04fc62ce80c7d26a7c001327c9699fca83
puakehaulani <puakehaulani@gmail.com>
2021-02-02 17:53:50 -0800

    added fontawesome react component, added icons to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithubAlt } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
     return (
         <footer className="footer mt-auto py-3">
-            <span>made with React</span>
+            <a href="https://github.com/puakehaulani"><FontAwesomeIcon icon={faGithubAlt} size="2x" /></a>
+            <a href="https://www.linkedin.com/in/lexijack"><FontAwesomeIcon icon={faLinkedinIn} size="2x" /></a>
+            <a href="https://www.instagram.com/puakehaulani/"><FontAwesomeIcon icon={faInstagram} size="2x" /></a>
         </footer>
     );
 }
