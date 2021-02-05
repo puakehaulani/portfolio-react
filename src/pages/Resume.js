4645ce15082ddcf7bc8d0e806bdf7a2694b9bfe6
puakehaulani <puakehaulani@gmail.com>
2021-02-05 11:51:28 -0800

    got filepaths for resume correct

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 export default function Resume() {
     return (
-        <div>
-            resume
-        </div>
+        <div className="row d-flex justify-content-center mt-5">
+            <div>
+                <a href={window.location.origin + "/resume.pdf"} download>
+                    <img src={window.location.origin + "/images/resumeimg.png"} />
+                    <br />
+                    click to download
+                </a>
+            </div>
+        </div >
     )
 }
