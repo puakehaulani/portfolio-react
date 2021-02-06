937879bdf5797e32100551d0e4d02a64dc7f2ddd
puakehaulani <puakehaulani@gmail.com>
2021-02-06 14:31:16 -0800

    window.location path update

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -4,8 +4,8 @@ export default function Resume() {
     return (
         <div className="row d-flex justify-content-center mt-5">
             <div>
-                <a href={window.location.origin + "/resume.pdf"} download>
-                    <img src={window.location.origin + "/images/resumeimg.png"} alt="resume" />
+                <a href={window.location.pathname + "/resume.pdf"} download>
+                    <img src={window.location.pathname + "/images/resumeimg.png"} alt="resume" />
                     <br />
                     click to download
                 </a>
