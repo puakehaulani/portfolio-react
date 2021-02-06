ead3d57ca7351ac6d4e360a130c2aee544878d82
puakehaulani <puakehaulani@gmail.com>
2021-02-06 14:36:36 -0800

    ./

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -4,8 +4,8 @@ export default function Resume() {
     return (
         <div className="row d-flex justify-content-center mt-5">
             <div>
-                <a href="/resume.pdf" download>
-                    <img src="/images/resumeimg.png" alt="resume" />
+                <a href="./resume.pdf" download>
+                    <img src="./images/resumeimg.png" alt="resume" />
                     <br />
                     click to download
                 </a>
