ec00a3fbbd74f9b9884b75fb8dc8525966c8c283
puakehaulani <puakehaulani@gmail.com>
2021-02-06 21:26:17 -0800

    fixed filepath to about

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home() {
     return (
         <div className="row d-flex justify-content-center entrance">
-            <a href="/about" className="text-center">aloha e honua
+            <a href="/portfolio-react/about" className="text-center">aloha e honua
             <br />
             hello world</a>
         </div>
